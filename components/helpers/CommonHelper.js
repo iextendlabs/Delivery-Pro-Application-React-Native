77f8d489724fe61156fdba0314813590826a2feb
Ali Hamza <miangdpp@gmail.com>
2023-07-31 19:24:43 +0500

    order links management

---
+++ components/helpers/CommonHelper.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed, so we add 1.
+const day = String(date.getDate()).padStart(2, '0');
+
+const formattedDateToday = `${year}-${month}-${day}`;
+
+export {formattedDateToday}
--- components/screens/OrderList.js
+++ components/screens/OrderList.js
@@ -23,6 +23,7 @@ import OrderCashCollectionModal from "./OrderCashCollectionModal";
 import Icon from "react-native-vector-icons/Ionicons";
 import { OrderStatusUpdateUrl } from "../config/Api";
 import LocationElement from "../modules/LocationElement";
+import { formattedDateToday } from "../helpers/CommonHelper";
 
 const OrderList = ({ initialParams }) => {
   const [orders, setOrders] = useState([]);
@@ -32,22 +33,23 @@ const OrderList = ({ initialParams }) => {
   const [detailsModalVisible, setDetailsModalVisible] = useState(false);
   const [commentModalVisible, setCommentModalVisible] = useState(false);
   const [actionModalVisible, setActionModalVisible] = useState(false);
-  const [cashCollectionModalVisible, setCashCollectionModalVisible] = useState(false);
+  const [cashCollectionModalVisible, setCashCollectionModalVisible] =
+    useState(false);
   const [selectedOrder, setSelectedOrder] = useState(null);
 
   const setSuccess = (message) => {
-    setSuccessMessage(message)
+    setSuccessMessage(message);
     setTimeout(() => {
       setSuccessMessage("");
     }, 2000);
-  }
+  };
 
   const setError = (message) => {
-    setErrorMessage(message)
+    setErrorMessage(message);
     setTimeout(() => {
       setErrorMessage("");
     }, 2000);
-  }
+  };
   const route = useRoute();
   var status = "Pending";
 
@@ -74,7 +76,20 @@ const OrderList = ({ initialParams }) => {
           OrderUrl + "status=" + orderStatus + "&user_id=" + userId
         );
         const data = await response.json();
-        setOrders(data);
+
+        // Get today's date in 'yyyy-mm-dd' format
+        const todayDate = formattedDateToday;
+
+        // Update the 'private' attribute based on the 'date' attribute
+        const updatedData = data.map((order) => {
+          return {
+            ...order,
+            private: (order.status !== 'Complete' && formattedDateToday === order.date),
+            todays_order: formattedDateToday === order.date
+          };
+        });
+        console.log(updatedData);
+        setOrders(updatedData);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching orders:", error);
@@ -89,19 +104,39 @@ const OrderList = ({ initialParams }) => {
   const renderOrder = ({ item }) => (
     <TouchableOpacity style={styles.orderContainer}>
       <View style={{ flex: 1.5 }}>
-        <Text style={styles.orderId}>ID: {item.id} <Icon name="ios-calendar" size={15} color="black" /> {item.date} </Text>
+        <Text style={styles.orderId}>
+          ID: {item.id} <Icon name="ios-calendar" size={15} color="black" />{" "}
+          {item.date}{" "}
+        </Text>
         <Text style={styles.orderDate}>{item.time_slot_value}</Text>
       </View>
 
       <View style={styles.OrderLinks}>
-        <LocationElement latitude={item.latitude} longitude={item.longitude} address={ item.buildingName + ' ' + item.street + ',' + item.area + ' ' + item.city} />
-        <Icon
-          name="eye"
-          size={25}
-          color="orange"
-          style={styles.icons}
-          onPress={() => handleOrderDetailPress(item)}
-        />
+      {item.status !== 'Complete' && 
+        
+        <LocationElement
+          latitude={item.latitude}
+          longitude={item.longitude}
+          address={
+            item.buildingName +
+            " " +
+            item.street +
+            "," +
+            item.area +
+            " " +
+            item.city
+          }
+          />
+        }
+        {item.private && (
+          <Icon
+            name="eye"
+            size={25}
+            color="orange"
+            style={styles.icons}
+            onPress={() => handleOrderDetailPress(item)}
+          />
+        )}
         {status !== "Complete" && (
           <Icon
             name="chatbubble-ellipses-outline"
@@ -111,12 +146,12 @@ const OrderList = ({ initialParams }) => {
             onPress={() => handleOrderCommentPress(item)}
           />
         )}
-        <PhoneNumber phoneNumber={item.number} />
-        {status === "Accepted" && (
+        {item.private && <PhoneNumber phoneNumber={item.number} />}
+        {status === "Accepted"   && item.todays_order && (
           <Icon
             name="md-hourglass"
             size={25}
-            color="orange"  // Change this to your desired color for 'Accepted' status.
+            color="orange" // Change this to your desired color for 'Accepted' status.
             style={styles.icons}
             onPress={() => handleInprogressOrder(item)}
           />
@@ -125,7 +160,7 @@ const OrderList = ({ initialParams }) => {
           <Icon
             name="md-checkmark-circle"
             size={25}
-            color="green"  // Change this to your desired color for 'Inprogress' status.
+            color="green" // Change this to your desired color for 'Inprogress' status.
             style={styles.icons}
             onPress={() => handleCompleteOrder(item)}
           />
@@ -134,7 +169,7 @@ const OrderList = ({ initialParams }) => {
           <Icon
             name="ellipsis-vertical"
             size={25}
-            color="blue"  // Change this to your desired color for 'Pending' status.
+            color="blue" // Change this to your desired color for 'Pending' status.
             style={styles.icons}
             onPress={() => handleOrderActionPress(item)}
           />
@@ -143,8 +178,7 @@ const OrderList = ({ initialParams }) => {
           <Icon
             name="cash-outline"
             size={25}
-
-            color={item.cash_status ? 'green' : 'orange'}
+            color={item.cash_status ? "green" : "orange"}
             style={styles.icons}
             onPress={() => handleOrderCashCollection(item)}
           />
