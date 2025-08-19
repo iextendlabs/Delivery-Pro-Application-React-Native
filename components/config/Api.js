a6f2294494b88ae87b9c46733ea53844d8a7f0c6
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2025-08-19 18:19:06 +0500

    Apps url updates to partner.lipslay.com from tadhem

--- components/config/Api.js
+++ components/config/Api.js
@@ -1,4 +1,4 @@
-const BaseUrl = "https://admin.tadhem.com/";
+const BaseUrl = "https://partner.lipslay.com/";
 
 const getStaffProfileUrl = BaseUrl + "api/getStaffProfile?";
 const IndexUrl = BaseUrl + "api/index?";
